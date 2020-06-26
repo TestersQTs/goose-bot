e0c4e9986ed1b3cc52fb52877f1993ac0e6772b1
KlukCZ <rostak247@seznam.cz>
2020-06-26 18:46:02 +0200

    Created some basic message events as well as json database of all responses from Goose

--- events/message.js
+++ events/message.js
@@ -1,7 +1,16 @@
 const Guild = require("../models/guild");
 const mongoose = require("mongoose");
 
+const fs = require("fs");
+const phrases = require("../goosePhrases.json");
+
 module.exports = async (client, message) => {
+
+  /*
+  * Event logic. This doesn't need to be touched. Goose events
+  * will be separated below.
+  */
+
   Guild.findOne(
     {
       guildID: message.guild.id,
@@ -11,7 +20,7 @@ module.exports = async (client, message) => {
 
       if (message.author.bot) return;
       if (!message.guild) return;
-      if (!message.content.startsWith(prefix)) return;
+      //if (!message.content.startsWith(prefix)) return;
 
       if (!message.member) message.member = await message.guild.fetchMember(message);
 
@@ -24,6 +33,19 @@ module.exports = async (client, message) => {
       if (!command) command = client.commands.get(client.aliases.get(cmd));
 
       if (command) command.run(client, message, args);
+
+      /*
+      * Pre-specified events. Might make this editable by the guild Administrators
+      */
+     var chance = Math.floor(Math.random() * 5) + 1;
+     var chanceActual = Math.floor(Math.random() * 5) + 1;
+     var events = Math.floor(Math.random() * 5) + 1;
+
+      if (chance === chanceActual && events === 1) message.channel.send(phrases["messageEvents"]["event1"]);
+      if (chance === chanceActual && events === 2) message.channel.send(phrases["messageEvents"]["event2"]);
+      if (chance === chanceActual && events === 3) message.channel.send(phrases["messageEvents"]["event3"]);
+      if (chance === chanceActual && events === 4) message.channel.send(phrases["messageEvents"]["event4"]);
+      if (chance === chanceActual && events === 5) message.channel.send(phrases["messageEvents"]["event5"]);
     }
   );
 };
